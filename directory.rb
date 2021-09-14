c0bda2bf31deac46844994d0c4f0c2782098be83
Lisa Mac <lisa@MacBook-Air-2.local>
2021-09-14 15:55:48 +0100

    Comments to Ruby code

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+#first we print the list of students
+
 puts "The students of Villain Academy"
 puts "-------------"
 puts "Dr Hannibal Lecter"
@@ -11,8 +13,9 @@ puts "Freddy Kreuger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
-
+#finally we print the total number of students
 print "Overall we have "
+#it's important that print() doesn't add new line characters
 print 11
 puts " great students"
 
