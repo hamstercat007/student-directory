a325289b79a421c801610507b5742bfd983e0624
Lisa Mac <lisa@MacBook-Air-2.local>
2021-09-15 17:41:51 +0100

    Step 5 Methods

--- directory.rb
+++ directory.rb
@@ -12,16 +12,24 @@ students = [
   "Joffrey Baratheon",
   "Norman Bates"
 ]
-# and then print them
+
+def print_header
   puts "The students of Villains Academy"
   puts "-------------"
-  students.each do |student| 
-    puts student
+end
+
+def print(names)
+  names.each do |name| 
+    puts name
   end
+end
 
-#finally we print the total
-  puts "Overall, we have #{students.count} great students"
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
 
-
+print_header
+print(students)
+print_footer(students)
 
 
