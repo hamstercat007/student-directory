a360310fc0033dd07da6cf632f9510bc78af702d
Lisa Mac <lisa@MacBook-Air-2.local>
2021-09-18 16:44:31 +0100

    Chapter 4 Step 8 Ex 8 - Month order

---
+++ Step8_8.rb
@@ -0,0 +1,96 @@
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # gets the first name
+  name = gets.chomp
+
+  # while the name is not empty, repeat this code
+  while !name.empty? do 
+    # add the student hash to the array 
+    # gets the cohort month
+    months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ]
+  
+    puts "What month is #{name} studying in?"
+
+    month = ""
+
+    loop do
+      month = gets.chomp
+      if months.include?(month)
+        break
+      elsif month == ""
+        month = "December"
+        break
+      else
+        puts "That must be a typo, please try again"
+      end
+    end
+    
+    students << {name: name, cohort: month.to_sym}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end 
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(students)
+  #print out each student hash grouped by cohort alphabetically
+  months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+  months.each do |month|
+    students.each {|student| puts "#{student[:name]}, #{student[:cohort]}" if student[:cohort] == month.to_sym } 
+  end
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
+end
+
+students = input_students
+# nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
+
+
+
+
+
+
+
+
